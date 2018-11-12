cbd07d1ad019d8523a29576618b56df86b1e0388
WojTechM <wojciech.m1997@gmail.com>
2018-11-12 16:59:06 +0100

    Change test signatures (new exception)

--- src/test/java/com/codecool/serverSide/ServerTest.java
+++ src/test/java/com/codecool/serverSide/ServerTest.java
@@ -1,6 +1,7 @@
 package com.codecool.serverSide;
 
 import com.codecool.model.EResult;
+import com.codecool.serverSide.exceptions.LackOfWorkersException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ServerTest {
 
     @Test
-    void Should_ReturnTrue_When_AllWorkersResultsAreValid() {
+    void Should_ReturnTrue_When_AllWorkersResultsAreValid() throws LackOfWorkersException {
         Server server = new Server(8000);
         Worker w1 = new Worker(null);
         w1.setResult(EResult.Valid);
@@ -21,7 +22,7 @@ class ServerTest {
     }
 
     @Test
-    void Should_ReturnFalse_When_OneOrMoreWorkersResultsAreInvalid() {
+    void Should_ReturnFalse_When_OneOrMoreWorkersResultsAreInvalid() throws LackOfWorkersException {
         Server server = new Server(8001);
         Worker w1 = new Worker(null);
         w1.setResult(EResult.Valid);
