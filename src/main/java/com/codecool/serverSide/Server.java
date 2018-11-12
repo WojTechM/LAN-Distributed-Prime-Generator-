ad5b3466c60b2cecaa3dfb31456cf354f4292b6b
WojTechM <wojciech.m1997@gmail.com>
2018-11-12 12:54:12 +0100

    Implement splitWorkIntoTasks method in Server class

--- src/main/java/com/codecool/serverSide/Server.java
+++ src/main/java/com/codecool/serverSide/Server.java
@@ -9,7 +9,6 @@ public class Server {
     private List<Worker> users;
     private List<Task> availableTasks;
 
-
     public Server(int port) {
         WorkerRegistration registration = new WorkerRegistration(this, port);
         Thread registrationThread = new Thread(registration);
@@ -17,11 +16,25 @@ public class Server {
     }
 
     public void run() {
-
+        int potentialPrime = 3;
+        while (!Thread.currentThread().isInterrupted()) {
+            splitWorkIntoTasks(potentialPrime);
+        }
     }
 
-    private void splitWorkIntoTasks() {
-
+    private void splitWorkIntoTasks(int potentialPrime) {
+        boolean reachedEnd = false;
+        int iteration = 0;
+        while (!reachedEnd) {
+            int from = iteration * 100;
+            int to = from + 100;
+            if (to > potentialPrime) {
+                to = potentialPrime;
+                reachedEnd = true;
+            }
+            Task task = new Task(potentialPrime, from, to);
+            availableTasks.add(task);
+        }
     }
 
     private void assignTasks() {
