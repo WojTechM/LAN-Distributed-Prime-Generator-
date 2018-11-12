f1525aec2c8ec29de2afa6028b3298829019b735
WojTechM <wojciech.m1997@gmail.com>
2018-11-12 11:28:07 +0100

    Implement Client class

--- src/main/java/com/codecool/clientSide/Client.java
+++ src/main/java/com/codecool/clientSide/Client.java
@@ -1,13 +1,75 @@
 package com.codecool.clientSide;
 
+import com.codecool.model.EResult;
 import com.codecool.model.Task;
 
-public class Client {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class Client implements Runnable {
 
     private String address;
     private int port;
 
-    public boolean partialCheck(Task task) {
-        return false;
+    public Client(String address, int port) {
+        this.address = address;
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        Socket connection;
+        ObjectInputStream inputStream;
+        ObjectOutputStream outputStream;
+        try {
+            connection = new Socket(address, port);
+            inputStream = new ObjectInputStream(connection.getInputStream());
+            outputStream = new ObjectOutputStream(connection.getOutputStream());
+
+        } catch (IOException e) {
+            System.out.println("Unable to connect with server.");
+            return;
+        }
+
+
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                Task task = (Task) inputStream.readObject();
+                EResult result = partialCheck(task);
+                outputStream.writeObject(result);
+            } catch (IOException | ClassNotFoundException e) {
+                System.out.println("Could not load task. Shutting down connection.");
+                break;
+            }
+        }
+
+        closeConnection(outputStream, connection);
+    }
+
+    private void closeConnection(ObjectOutputStream outputStream, Socket connection) {
+        try {
+            outputStream.writeObject(EResult.Disconnected);
+            connection.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public EResult partialCheck(Task task) {
+        boolean isPrime = false;
+        int toCheck = task.getPotentialPrime();
+        for(int i = task.getFrom(); i <= toCheck / 2; ++i)
+        {
+
+            if(toCheck % i == 0)
+            {
+                isPrime = true;
+                break;
+            }
+        }
+
+        return isPrime ? EResult.Valid : EResult.Invalid;
     }
 }
