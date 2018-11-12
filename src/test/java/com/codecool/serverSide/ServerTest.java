d72e2610cfdb4e6a5f6da66a6a8ecb987df36f94
WojTechM <wojciech.m1997@gmail.com>
2018-11-12 15:16:19 +0100

    Create tests for server side

---
+++ src/test/java/com/codecool/serverSide/ServerTest.java
@@ -0,0 +1,35 @@
+package com.codecool.serverSide;
+
+import com.codecool.model.EResult;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServerTest {
+
+    @Test
+    void Should_ReturnTrue_When_AllWorkersResultsAreValid() {
+        Server server = new Server(8000);
+        Worker w1 = new Worker(null);
+        w1.setResult(EResult.Valid);
+        Worker w2 = new Worker(null);
+        w2.setResult(EResult.Valid);
+        server.addWorker(w1);
+        server.addWorker(w2);
+
+        assertTrue(server.validateResult());
+    }
+
+    @Test
+    void Should_ReturnFalse_When_OneOrMoreWorkersResultsAreInvalid() {
+        Server server = new Server(8001);
+        Worker w1 = new Worker(null);
+        w1.setResult(EResult.Valid);
+        Worker w2 = new Worker(null);
+        w2.setResult(EResult.Invalid);
+        server.addWorker(w1);
+        server.addWorker(w2);
+
+        assertFalse(server.validateResult());
+    }
+}
