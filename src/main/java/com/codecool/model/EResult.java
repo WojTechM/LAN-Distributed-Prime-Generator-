540fadab9621d716fa7f57499e079cbdd543c61a
WojTechM <wojciech.m1997@gmail.com>
2018-11-12 12:25:25 +0100

    Implement run method in Worker class

--- src/main/java/com/codecool/model/EResult.java
+++ src/main/java/com/codecool/model/EResult.java
@@ -1,5 +1,5 @@
 package com.codecool.model;
 
 public enum EResult {
-    Valid, Invalid, Disconnected;
+    Valid, Invalid, Disconnected, InProgress
 }
--- src/main/java/com/codecool/serverSide/Worker.java
+++ src/main/java/com/codecool/serverSide/Worker.java
@@ -3,6 +3,9 @@ package com.codecool.serverSide;
 import com.codecool.model.EResult;
 import com.codecool.model.Task;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class Worker implements Runnable {
@@ -13,6 +16,7 @@ public class Worker implements Runnable {
 
     public Worker(Socket socket) {
         this.socket = socket;
+        this.result = EResult.InProgress;
     }
 
     public EResult getResult() {
@@ -21,18 +25,32 @@ public class Worker implements Runnable {
 
     @Override
     public void run() {
+        ObjectInputStream inputStream;
+        ObjectOutputStream outputStream;
+        try {
+            inputStream = new ObjectInputStream(socket.getInputStream());
+            outputStream = new ObjectOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            System.out.println("Could not connect to socket.");
+            result = EResult.Disconnected;
+            return;
+        }
 
+        while (!result.equals(EResult.Disconnected)) {
+            waitForTask();
+            handleTask(inputStream, outputStream);
+        }
     }
 
     public void assignTask(Task task) {
-
+        this.task = task;
     }
 
     private void waitForTask() {
 
     }
 
-    private void handleTask() {
+    private void handleTask(ObjectInputStream inputStream, ObjectOutputStream outputStream) {
 
     }
 }
