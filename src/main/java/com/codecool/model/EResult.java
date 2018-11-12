088f389aa8217790076849fc1b118a7be607e2ac
WojTechM <wojciech.m1997@gmail.com>
2018-11-12 16:56:30 +0100

    Create new state - 'New'

--- src/main/java/com/codecool/model/EResult.java
+++ src/main/java/com/codecool/model/EResult.java
@@ -3,5 +3,5 @@ package com.codecool.model;
 import java.io.Serializable;
 
 public enum EResult implements Serializable {
-    Valid, Invalid, Disconnected, InProgress
+    Valid, Invalid, Disconnected, InProgress, New
 }
